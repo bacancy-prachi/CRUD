69070a73b7dce2e7369c34cdec2da56d5870d812
Prachi <prachi.jain@bacancytechnology.com>
2020-01-30 11:49:51 +0530

    crud first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :products  
+  root 'products#index' 
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
